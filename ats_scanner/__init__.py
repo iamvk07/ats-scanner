ed54940e3660a331e79ccf1289a2ec14e2d9a82f
Vedant Kadam <j89tj@unb.ca>
2026-03-23 01:43:38 -0300

    Create __init__.py

---
+++ ats_scanner/__init__.py
@@ -0,0 +1,8 @@
+"""
+ATS Scanner — Resume vs Job Description Analyzer
+Zero dependencies. Pure Python 3.8+
+"""
+
+__version__ = "1.0.0"
+__author__ = "Vedant Kadam"
+__github__ = "github.com/iamvk07/ats-scanner"
